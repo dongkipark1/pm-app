ee659102beca7ac1d122bfff3c5b9a423ecf8a28
qkrehdrl4417 <qkrehdrl4417@naver.com>
2025-04-23 17:43:00 +0900

    user 엔티티 설계

--- src/main/java/com/example/pmapp/dto/User.java
+++ src/main/java/com/example/pmapp/dto/User.java
@@ -1,4 +1,19 @@
 package com.example.pmapp.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role; // "관리자" and "직원"
 }
