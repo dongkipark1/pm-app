c3dfed5bd2e2a5f2d76f4a304f311a1bcfab4967
qkrehdrl4417 <qkrehdrl4417@naver.com>
2025-04-25 16:53:01 +0900

    security config 설정 파일

---
+++ src/main/java/com/example/pmapp/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.pmapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http
+               .csrf(csrf -> csrf.disable())
+               .authorizeHttpRequests(auth -> auth
+                       .requestMatchers("/", "/users/join", "/css/**").permitAll()
+                       .anyRequest().authenticated()
+               )
+               .formLogin(login -> login
+                       .loginPage("/login")
+                       .defaultSuccessUrl("/products", true)
+                       .permitAll()
+               )
+               .logout(logout -> logout
+                       .logoutSuccessUrl("/login?logout")
+                       .permitAll()
+               );
+       return http.build();
+    }
+}
